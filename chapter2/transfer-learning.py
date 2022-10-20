141206eeb489b010e4668478ecf915c86b4d121b
Sourcery AI <>
2022-10-20 11:34:07 +0000

    'Refactored by Sourcery'

--- chapter2/transfer-learning.py
+++ chapter2/transfer-learning.py
@@ -56,12 +56,12 @@ def split_data(SOURCE, TRAINING, TESTING, SPLIT_SIZE):
         if os.path.getsize(file) > 0:
             files.append(filename)
         else:
-            print(filename + " is zero length, so ignoring.")
+            print(f"{filename} is zero length, so ignoring.")
 
     training_length = int(len(files) * SPLIT_SIZE)
     testing_length = int(len(files) - training_length)
     shuffled_set = random.sample(files, len(files))
-    training_set = shuffled_set[0:training_length]
+    training_set = shuffled_set[:training_length]
     testing_set = shuffled_set[:testing_length]
 
     for filename in training_set:
